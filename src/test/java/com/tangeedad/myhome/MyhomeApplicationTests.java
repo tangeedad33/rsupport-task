f6c10d5fe45e1cf3705fe8e339e4413680b50acd
tangeedad <tangeedad@gmail.com>
2024-12-30 21:46:04 +0900

    init

---
+++ src/test/java/com/tangeedad/myhome/MyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangeedad.myhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
